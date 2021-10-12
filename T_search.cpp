5b5e1fe4aba980a2391ade1d23f9c565ea617d23
Komi_op87@stud.msk.itstep.org <Komi_op87@stud.msk.itstep.org>
2021-10-12 21:46:20 +0300

    Добавьте файлы проекта.

---
+++ T_search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*Реализуйте шаблонную функцию для двоичного поиска в массиве
+Протестируйте на массиве строк и чисел.
+Строки читать из файла до конца потока!
+Числа генерить случайно*/
+
+template <typename T>
+T search(T* arr, int size) {
+	for (int i = 0; i < size; i++) {
+
+	}
+}
+
+int main() {
+	int size = 20;
+	int* Arr = new int[size];
+}
